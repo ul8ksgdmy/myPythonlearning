edb6edafe20b766734067bac34f96e77759fc80b
ul8ksgdmy <clickmy7@gmail.com>
2018-10-18 20:45:06 +0900

    mongdb practice with python

--- py1810/hello_mongo_practice.py
+++ py1810/hello_mongo_practice.py
@@ -3,22 +3,26 @@ import json
 import sys
 
 #접속 실행전 myinfo 경로확인
-with open('D:\py\myPythonlearning\py1810\myinfo.json') as f: 
+with open('C:\\Users\W7\python-workspace\myPythonpractice\DB\myinfo.json') as f: 
     data = json.load(f)
 
-ip = data['ip']
-port = data['port']
+host = data['host']
+port = data['mongoport']
 
-client = MongoClient(ip, port)
+client = MongoClient(host, port)
 db = client.get_database('mongodb')
 collection = db.get_collection('restaurants')
 
 #문제번호
-ask = sys.argv[1]
+try:
+    ask = sys.argv[1]
+except:
+    ask = '13'
 
 #query 초기화 및 cursor
 query = {}
 uquery = {}
+dquery = {}
 newvalues = {}
 
 try:
@@ -40,27 +44,43 @@ try:
         query = {'$or':[{'address.zipcode':'11209'},{'cuisine':'Italian'}]}
     elif ask == '9': #9. 이름이 ‘Juni’인 음식점을 찾아서 요리cuisine를 ‘American (New)’으로 변경하라
         uquery = {'name':'Indian Oven'}
-        newvalues = { "$set": {'cuisine':'American (new)'} }
-    else:
+        newvalues = { '$set': {'cuisine':'American'} }
+    elif ask == '10': #10. 음식점 아이디가 ‘41156888’인 음식점을 찾아 주소address를 ‘East 31st Street’으로 변경하라
+        uquery = {'restaurant_id':'50018995'}
+        newvalues = {'$set': {'address.street':'East 33st Street'}}
+    elif ask == '11': #11. 우편번호가 10016이고, 요리cuisine가 ‘Other’인 음식점을 찾아서 요리cuisine를 ‘Category To Be Determined’로 변경하라
+        uquery = {'address.zipcode':'11218','cuisine':'Other'}
+        newvalues = {'$set':{'cuisine':'Category To Be Determined'}}
+    elif ask == '12': #12. 맨하튼Manhattan 자치구borough에 있는 모든 음식점을 모두 삭제하라
+        dquery = {'borough':'Brooklyn'}
+    elif ask == '13': #13. 자치구borough별 음식점 개수를 조회하라
+        param = [{'$group':{'_id':'$borough', 'total' :{'$sum':1}}}]
+    elif ask == '14': #14. Queens 자치구 내 Brazilian 요리 음식점이 구역별zipcode로 얼마나 존재하는지 조회하라
+        param = [{'$group':{'_id':'address.zip','total':{'$sum':1}}}] #모르겠다 일단 자자
+        match = [{'$match':{'borough':'Queens','cuisine':'Brazilian'}}]
+    else: #
         query = {}
 except:
     pass
 
-cs = collection.find(query)
-up = collection.update_one(uquery,newvalues)
+#find를 사용하는 것만 별도로 출력하도록 함. (출력량이 많기 때문)
+if int(ask) < 9:
+    cs = collection.find(query)
+    # 출력
+    for i in cs:
+        print(i)
 
-#출력
-# for i in cs:
-#     print(i)
+    print(cs.count)
+elif int(ask) < 11:
+    up = collection.update_one(uquery, newvalues)
 
-print(cs.count())
-print(up.upserted_id)
+elif int(ask) == 12:
+    rm = collection.delete_one(dquery)
+else:
+    csag = collection.aggregate(param)
 
-#cursor 갯수
-# print(ask)
-# print(query)
-
-
+for i in csag:
+    print(i)
 
 
 #cusrsor 종료
